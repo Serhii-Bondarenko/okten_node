127bf3afe74de1ff894653d2225b3981c7c772be
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-02-22 23:05:16 +0200

    set config for node js

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { users } from './users';
+
+const app = express();
+
+console.log(users);
+
+app.listen(5500, () => {
+    console.log('Server has been started!');
+});
